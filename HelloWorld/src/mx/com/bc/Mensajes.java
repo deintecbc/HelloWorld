7acc3533857d9d72de8325834a1c781d401c3794
Carlos Morales <carlmog@gmail.com>
2016-12-29 13:05:33 -0600

    Nuevo proyecto HelloWorld

---
+++ HelloWorld/src/mx/com/bc/Mensajes.java
@@ -0,0 +1,16 @@
+package mx.com.bc;
+
+public class Mensajes {
+
+	public static String saludo(){
+		return "Hola, prueba para Gerrit!!!";
+	}
+	
+	
+	
+	public static void cuenta(){
+		for(int i=5; i!=0 ; i--){
+			System.out.println(i);
+		}
+	}
+}
---
+++ HelloWorld/src/mx/com/bc/Principal.java
@@ -0,0 +1,12 @@
+package mx.com.bc;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Mensajes.cuenta();
+		System.out.println(Mensajes.saludo());
+		
+
+	}
+}
